b0661ed0479a4464cfdf4ee7b91e0c1966265ec2
frazier_g <garyfrazier95@yahoo.com>
2024-06-01 12:07:43 -0400

    fix keymap

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,7 +10,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
 	-- Replace the language servers listed here
 	-- with the ones you want to install
-	ensure_installed = {'tsserver', 'eslint', 'rust_analyzer', 'gopls', 'pyright'},
+	ensure_installed = {'tsserver', 'eslint', 'rust_analyzer', 'pyright', 'zls'},
 	handlers = {
 		function(server_name)
 			require('lspconfig')[server_name].setup({})
@@ -20,6 +20,14 @@ require('mason-lspconfig').setup({
 
 lsp_zero.set_preferences({
 	sign_icons = {}
+})
+
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<TAB>'] = cmp.mapping.confirm({select = false}),
+  }),
 })
 
 lsp_zero.setup()
